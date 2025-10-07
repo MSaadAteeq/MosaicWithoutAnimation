8a79833bb8475a0272824d758aaf80a717144d99
mramzan2023 <mramzan@vinncorp.com>
2025-10-07 12:12:45 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const light = document.querySelector('.light')
+const grid = document.querySelector('#hex-grid')
+grid.addEventListener('mousemove', (e) => {
+  light.style.left = `${e.clientX}px`
+  light.style.top = `${e.clientY}px`
+})

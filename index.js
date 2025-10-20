e7926112b1b574af9e8fddb4e7b3a28e0fb76471
Saad <saadateeq@vinncorp.com>
2025-10-20 20:08:20 +0500

    all work without animation

--- index.js
+++ index.js
@@ -5,55 +5,54 @@ import navigationToggle from './navigationToggle.js';
 import pointerAnimation from './pointerAnimation.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   const isHome = document.querySelector('.home');
   pointerAnimation()
   // scrollAnimation()
   navigationToggle()
 
 
-  if (isHome) {
-    new Swiper('.blogSwiper', {
-      
-      speed: 800,
-      slidesPerView: 1.5,
-      centeredSlides: true,
-      slidesOffsetBefore: 0,
-      slidesOffsetAfter: 0,
-      spaceBetween: 30,
-      grabCursor: true, 
-      // loop: true,
-      useCSS3D: true,
-      useCSS3DTransform: true,
-      on: {
-        init(swiper) {
-          // remove initial left offset added by centeredSlides
-          swiper.setTranslate(0)
-        }
+  new Swiper('.blogSwiper', {
+
+    speed: 800,
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    spaceBetween: 30,
+    grabCursor: true,
+    // loop: true,
+    useCSS3D: true,
+    useCSS3DTransform: true,
+    on: {
+      init(swiper) {
+        // remove initial left offset added by centeredSlides
+        swiper.setTranslate(0)
+      }
+    },
+
+    // Full width
+    // width: '100vw', 
+    navigation: {
+      nextEl: '.blog-button-next',
+      prevEl: '.blog-button-prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
       },
-      
-      // Full width
-      // width: '100vw', 
-      navigation: {
-        nextEl: '.blog-button-next',
-        prevEl: '.blog-button-prev',
+      1024: {
+        slidesPerView: 2.5,
+        spaceBetween: 50,
       },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 2.5,
-          spaceBetween: 50,
-        },
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 60,
-        },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 60,
       },
-    });
-  }
+    },
+  });
+
 
 })
 
